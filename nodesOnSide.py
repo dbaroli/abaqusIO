918532cc56859ff2253ca43dc3a00a262476968f
dbaroli <80335267+dbaroli@users.noreply.github.com>
2021-06-30 18:56:39 +0200

    Update nodesOnSide.py

--- nodesOnSide.py
+++ nodesOnSide.py
@@ -1,9 +1,12 @@
 from abaqus import * 
 import numpy as np
-pmodel= mdb.ModelFromInputFile(name='F1', inputFileName='C:/Users/elidi/Documents/Abaqus_job/cohesive_model/F1.inp')
+import os 
+homefolder= os.getcwd()
+pathfile=os.path.join(homefolder,'F1.inp')
+pmodel= mdb.ModelFromInputFile(name='F1', inputFileName=pathfile)
 ##Nodes
 setCZ=pmodel.rootAssembly.sets['CONTRUZ']
 NodesCZ=[]
 for node in setCZ.nodes:
     NodesCZ.append([node.label,node.coordinates[0],node.coordinates[1],node.coordinates[2]])
-np.savetxt("C:/Users/elidi/Documents/Abaqus_job/cohesive_model/nodes_cz.txt",np.asarray(NodesCZ),delimiter=',')
+np.savetxt("nodes_cz.txt",np.asarray(NodesCZ),delimiter=',')
